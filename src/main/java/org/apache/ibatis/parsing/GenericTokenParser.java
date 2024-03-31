63c2af9b46847bfb1bc1b4bcbf104f9db5963f27
forever <346331488@qq.com>
2024-03-31 17:33:53 +0800

    add: 添加代码中文注释

--- src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
+++ src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
@@ -20,7 +20,13 @@ package org.apache.ibatis.parsing;
  */
 public class GenericTokenParser {
 
+  /**
+   * 开始的 Token 字符串
+   */
   private final String openToken;
+  /**
+   * 结束的 Token 字符串
+   */
   private final String closeToken;
   private final TokenHandler handler;
 
@@ -30,58 +36,96 @@ public class GenericTokenParser {
     this.handler = handler;
   }
 
+  /**
+   * 此方法用于解析给定的字符串，查找并替换由openToken和closeToken标识的表达式。
+   * 当发现非转义的标记对时，将调用handler对象的handleToken方法处理标记对之间的内容，
+   * 并将处理结果插入到原始文本对应位置。如果标记前后存在转义字符，则忽略其作为标记的作用。
+   *
+   * @param text 输入的待解析字符串
+   * @return 经过处理后的新字符串
+   */
   public String parse(String text) {
     if (text == null || text.isEmpty()) {
       return "";
     }
     // search open token
+    // 寻找开始的 openToken 的位置
     int start = text.indexOf(openToken);
     if (start == -1) {
       return text;
     }
     char[] src = text.toCharArray();
+    // 起始查找位置
     int offset = 0;
     final StringBuilder builder = new StringBuilder();
+    // 匹配到 openToken 和 closeToken 之间的表达式
     StringBuilder expression = null;
+    // 循环匹配
     while (start > -1) {
+      // 转义字符
       if (start > 0 && src[start - 1] == '\\') {
         // this open token is escaped. remove the backslash and continue.
+        // 因为 openToken 前面一个位置是 \ 转义字符，所以忽略 \
+        // 添加 [offset, start - offset - 1] 和 openToken 的内容，添加到 builder 中
         builder.append(src, offset, start - offset - 1).append(openToken);
+        // 修改 offset
         offset = start + openToken.length();
+        // 非转义字符
       } else {
         // found open token. let's search close token.
+        // 创建/重置 expression 对象
         if (expression == null) {
           expression = new StringBuilder();
         } else {
           expression.setLength(0);
         }
+        // 添加 offset 和 openToken 之间的内容，添加到 builder 中
         builder.append(src, offset, start - offset);
+        // 修改 offset
         offset = start + openToken.length();
+        // 寻找结束的 closeToken 的位置
         int end = text.indexOf(closeToken, offset);
         while (end > -1) {
+          // 转义
           if (end > offset && src[end - 1] == '\\') {
             // this close token is escaped. remove the backslash and continue.
+            // 当前closeToken前有转义字符，移除该反斜杠并继续
+            // 因为 endToken 前面一个位置是 \ 转义字符，所以忽略 \
+            // 添加 [offset, end - offset - 1] 和 endToken 的内容，添加到 builder 中
             expression.append(src, offset, end - offset - 1).append(closeToken);
+            // 修改 offset
             offset = end + closeToken.length();
+            // 继续，寻找结束的 closeToken 的位置
             end = text.indexOf(closeToken, offset);
           } else {
+            // 添加 [offset, end - offset] 的内容，添加到 builder 中
             expression.append(src, offset, end - offset);
             offset = end + closeToken.length();
             break;
           }
         }
+        // 拼接内容
         if (end == -1) {
           // close token was not found.
+          // closeToken 未找到，直接拼接,
+          // 未找到closeToken，将剩余文本原样追加
           builder.append(src, start, src.length - start);
+          // 修改 offset
           offset = src.length;
         } else {
+          // <x> closeToken 找到，将 expression 提交给 handler 处理 ，并将处理结果添加到 builder 中
+          // 找到了匹配的closeToken，处理表达式内容并追加结果
           builder.append(handler.handleToken(expression.toString()));
+          // 修改 offset
           offset = end + closeToken.length();
         }
       }
+      // 继续，寻找开始的 openToken 的位置
       start = text.indexOf(openToken, offset);
     }
+    // 拼接剩余的部分
     if (offset < src.length) {
+      // 处理完所有标记对后，追加剩余的未处理文本
       builder.append(src, offset, src.length - offset);
     }
     return builder.toString();
--- src/main/java/org/apache/ibatis/parsing/XPathParser.java
+++ src/main/java/org/apache/ibatis/parsing/XPathParser.java
@@ -45,10 +45,25 @@ import org.xml.sax.SAXParseException;
  */
 public class XPathParser {
 
+  /**
+   * XML Document 对象
+   */
   private final Document document;
+  /**
+   * 是否校验
+   */
   private boolean validation;
+  /**
+   * XML实体解析器
+   */
   private EntityResolver entityResolver;
+  /**
+   * 变量
+   */
   private Properties variables;
+  /**
+   * java Xpath 对象
+   */
   private XPath xpath;
 
   public XPathParser(String xml) {
@@ -111,6 +126,14 @@ public class XPathParser {
     this.document = document;
   }
 
+  /**
+   * 构造 XPathParser 对象
+   *
+   * @param xml XML 文件地址
+   * @param validation 是否校验 XML
+   * @param variables 变量 Properties 对象
+   * @param entityResolver XML 实体解析器
+   */
   public XPathParser(String xml, boolean validation, Properties variables, EntityResolver entityResolver) {
     commonConstructor(validation, variables, entityResolver);
     this.document = createDocument(new InputSource(new StringReader(xml)));
@@ -140,7 +163,9 @@ public class XPathParser {
   }
 
   public String evalString(Object root, String expression) {
+    // <1> 获得值
     String result = (String) evaluate(expression, root, XPathConstants.STRING);
+    // <2> 基于 variables 替换动态值，如果 result 为动态值
     result = PropertyParser.parse(result, variables);
     return result;
   }
@@ -193,14 +218,18 @@ public class XPathParser {
     return (Double) evaluate(expression, root, XPathConstants.NUMBER);
   }
 
+  // Node 数组
   public List<XNode> evalNodes(String expression) {
     return evalNodes(document, expression);
   }
 
+  // Node 数组
   public List<XNode> evalNodes(Object root, String expression) {
     List<XNode> xnodes = new ArrayList<>();
+    // <1> 获得 Node 数组
     NodeList nodes = (NodeList) evaluate(expression, root, XPathConstants.NODESET);
     for (int i = 0; i < nodes.getLength(); i++) {
+      // <2> 封装成 XNode 数组
       xnodes.add(new XNode(this, nodes.item(i), variables));
     }
     return xnodes;
@@ -211,13 +240,23 @@ public class XPathParser {
   }
 
   public XNode evalNode(Object root, String expression) {
+    // <1> 获得 Node 对象
     Node node = (Node) evaluate(expression, root, XPathConstants.NODE);
     if (node == null) {
       return null;
     }
+    // <2> 封装成 XNode 对象
     return new XNode(this, node, variables);
   }
 
+  /**
+   * 获得指定元素或节点的值
+   *
+   * @param expression 表达式
+   * @param root 指定节点
+   * @param returnType 返回类型
+   * @return 值
+   */
   private Object evaluate(String expression, Object root, QName returnType) {
     try {
       return xpath.evaluate(expression, root, returnType);
@@ -226,10 +265,19 @@ public class XPathParser {
     }
   }
 
+  /**
+   * 创建 Document 对象
+   *
+   * @param inputSource XML 的 InputSource 对象
+   * @return Document 对象
+   */
   private Document createDocument(InputSource inputSource) {
     // important: this must only be called AFTER common constructor
+    // 重要：这只能调用公共构造函数之后
     try {
+      // 1> 创建 DocumentBuilderFactory 对象
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      // 设置是否验证 XML
       factory.setValidating(validation);
 
       factory.setNamespaceAware(false);
@@ -238,8 +286,11 @@ public class XPathParser {
       factory.setCoalescing(false);
       factory.setExpandEntityReferences(true);
 
+      // 2> 创建 DocumentBuilder 对象
       DocumentBuilder builder = factory.newDocumentBuilder();
+      // 设置实体解析器
       builder.setEntityResolver(entityResolver);
+      // 实现都空的
       builder.setErrorHandler(new ErrorHandler() {
         @Override
         public void error(SAXParseException exception) throws SAXException {
@@ -255,6 +306,7 @@ public class XPathParser {
         public void warning(SAXParseException exception) throws SAXException {
         }
       });
+      // 3> 解析 XML 文件
       return builder.parse(inputSource);
     } catch (Exception e) {
       throw new BuilderException("Error creating document instance.  Cause: " + e, e);
@@ -265,6 +317,7 @@ public class XPathParser {
     this.validation = validation;
     this.entityResolver = entityResolver;
     this.variables = variables;
+    // 创建 XPathFactory 对象
     XPathFactory factory = XPathFactory.newInstance();
     this.xpath = factory.newXPath();
   }
